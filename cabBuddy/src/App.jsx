6cd0f29225209169192ad7a55f9a7408d63ac9f8
AKD45 <akashdidore2001@gmail.com>
2025-11-06 10:05:58 +0530

    Feat: Project setup vite | ShadCn | TailWindCSS

---
+++ cabBuddy/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col h-screen  gap-8 justify-center items-center'>
+      this is cabBuddy
+      <Button>
+        click me 
+      </Button>
+    </div>
+    </>
+  )
+}
+
+export default App
