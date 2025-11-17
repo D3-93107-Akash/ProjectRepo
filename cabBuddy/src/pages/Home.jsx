e6edc47984c71020801aa09c2a2abc6467fa3f1c
AKD45 <akashdidore2001@gmail.com>
2025-11-17 23:11:04 +0530

    Commit by Akash Didore

--- cabBuddy/src/pages/Home.jsx
+++ cabBuddy/src/pages/Home.jsx
@@ -1,13 +1,32 @@
+"use client";
+
+import SearchBox from "@/components/searchBox";
+import FeatureCards from "@/components/featureCard";
+
 export default function Home() {
-    return (
-      <div className="p-8 text-center">
-        <h1 className="text-3xl font-bold text-[#003366] mb-2">
-          Welcome to CabBuddy
+  return (
+    <div className="w-full">
+
+      {/* Hero Section with Background Image */}
+      <div
+        className="h-[70vh] w-full bg-cover bg-center flex flex-col items-center justify-center text-center pt-40"
+        style={{
+          backgroundImage: "url('/assets/pic1.svg')",
+        }}
+      >
+        <h1 className="text-5xl font-bold text-white mb-2">
+         Quality rides that fit your budget
         </h1>
-        <p className="text-gray-600">
-          Your trusted carpool and cab-sharing partner 🚗
+        <p className="text-white text-lg mb-55">
+          Book your cab quickly and easily
         </p>
+
+        {/* Your Existing Search Box */}
+        <SearchBox />
       </div>
-    );
-  }
-  
+
+      {/* Feature Cards Section */}
+      <FeatureCards />
+    </div>
+  );
+}
