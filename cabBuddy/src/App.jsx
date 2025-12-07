d9c9ef53c62afdc6f0c9a61652175aca75d97694
AKD45 <akashdidore2001@gmail.com>
2025-12-07 18:59:53 +0530

    Committed by AkashD

--- cabBuddy/src/App.jsx
+++ cabBuddy/src/App.jsx
@@ -5,18 +5,30 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import PublishRide from "./pages/AddRide";
 import Payments from "./pages/Payments";
+import PickupPage from "./pages/Pickup";
+import DropoffPage from "./pages/DropOff";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // keep this once in your app [web:39][web:117]
+import SelectRoute from "./pages/SelectRoute";
 
 function App() {
   return (
     <>
-      <Navbar  />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/publish-ride" element={<PublishRide />} />
         <Route path="/my-payments" element={<Payments />} />
-        
+        <Route path="/pickup" element={<PickupPage/>}></Route>
+        <Route path="/drop-off" element={<DropoffPage/>}></Route>
+        <Route path="/select-route" element={<SelectRoute/>} ></Route>
       </Routes>
+
+      {/* Toast container at root */}
+      {/* <ToastContainer position="top-right" autoClose={3000} /> */}
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </>
   );
 }
